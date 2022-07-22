dae6bae032496af83bfb239046e4e2042a85b58f
Brad Sheppard <bradsheppard22@gmail.com>
2022-07-21 22:28:18 -0230

    Module updates

--- main.tf
+++ main.tf
@@ -1,122 +1,154 @@
-resource "random_id" "id" {
-    byte_length = 4
-    prefix      = var.project
-}
-
-resource "google_project" "project" {
-    name       = var.project
-    project_id = random_id.id.hex
-    auto_create_network = false
-    billing_account = var.billing_acccount
-}
-
-resource "google_project_service" "gke-api" {
-    project = google_project.project.project_id
-    service = "container.googleapis.com"
-}
-
-resource "google_compute_network" "vpc" {
-    name                    = "${var.project}-vpc"
-    auto_create_subnetworks = "false"
-    project = google_project.project.project_id
-}
-
-resource "google_compute_subnetwork" "private-subnet" {
-    name          = "${var.project}-private-subnet"
-    ip_cidr_range = "10.0.0.0/24"
-    network       = google_compute_network.vpc.name
-    depends_on    = [google_compute_network.vpc]
-    region        = var.location
-    private_ip_google_access = "true"
-    project = google_project.project.project_id
-}
-
-resource "google_compute_router" "router" {
-    name    = "${var.project}-router"
-    project = google_project.project.project_id
-    region  = var.location
-    network = google_compute_network.vpc.self_link
-
-    bgp {
-        asn = 64514
-    }
-}
-
-resource "google_compute_router_nat" "nat" {
-    name                               = "${var.project}-router-nat"
-    router                             = google_compute_router.router.name
-    region                             = google_compute_router.router.region
-    nat_ip_allocate_option             = "AUTO_ONLY"
-    source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-    project = google_project.project.project_id
-
-    log_config {
-        enable = true
-        filter = "ERRORS_ONLY"
-    }
-}
-
 resource "google_container_cluster" "primary" {
-    name     = "${var.project}-gke-cluster"
-    location = var.zone
+  name     = "${var.name}-gke-cluster"
+  location = var.zone
 
-    # We can't create a cluster with no node pool defined, but we want to only use
-    # separately managed node pools. So we create the smallest possible default
-    # node pool and immediately delete it.
-    remove_default_node_pool = true
-    initial_node_count       = 1
+  provider = google-beta
+  project = var.project_id
 
-    project = google_project.project.project_id
+  enable_tpu = true
 
-    ip_allocation_policy {
-        # Whether alias IPs will be used for pod IPs in the cluster. Defaults to
-        # true if the ip_allocation_policy block is defined, and to the API
-        # default otherwise. Prior to June 17th 2019, the default on the API is
-        # false; afterwards, it's true.
-    }
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
 
-    private_cluster_config {
-        # Whether the master's internal IP address is used as the cluster endpoint.
-        enable_private_endpoint = false
+  cluster_autoscaling {
+    enabled             = false
+    autoscaling_profile = "OPTIMIZE_UTILIZATION"
+  }
 
-        # Whether nodes have internal IP addresses only. If enabled, all nodes are
-        # given only RFC 1918 private addresses and communicate with the master via
-        # private networking.
-        enable_private_nodes = true
-        master_ipv4_cidr_block = "10.0.1.0/28"
-    }
+  ip_allocation_policy {
+    # Whether alias IPs will be used for pod IPs in the cluster. Defaults to
+    # true if the ip_allocation_policy block is defined, and to the API
+    # default otherwise. Prior to June 17th 2019, the default on the API is
+    # false; afterwards, it's true.
+  }
 
-    network = google_compute_network.vpc.id
-    subnetwork = google_compute_subnetwork.private-subnet.id
+  private_cluster_config {
+    # Whether the master's internal IP address is used as the cluster endpoint.
+    enable_private_endpoint = false
 
-    master_auth {
-        username = ""
-        password = ""
+    # Whether nodes have internal IP addresses only. If enabled, all nodes are
+    # given only RFC 1918 private addresses and communicate with the master via
+    # private networking.
+    enable_private_nodes   = true
+    master_ipv4_cidr_block = "10.0.1.0/28"
+  }
 
-        client_certificate_config {
-            issue_client_certificate = false
-        }
-    }
+  network    = var.vpc_id
+  subnetwork = var.subnet_id
+}
+
+resource "google_service_account" "node_service_account" {
+  account_id   = "gke-node-service-account"
+  display_name = "gke-node-service-account"
+}
+
+resource "google_project_iam_member" "cluster_service_account-log_writer" {
+  project = var.project_id
+  role   = "roles/logging.logWriter"
+  member = "serviceAccount:${google_service_account.node_service_account.email}"
+}
+
+resource "google_project_iam_member" "cluster_service_account-metric_writer" {
+  project = var.project_id
+  role   = "roles/monitoring.metricWriter"
+  member = "serviceAccount:${google_service_account.node_service_account.email}"
+}
+
+resource "google_project_iam_member" "cluster_service_account-monitoring_viewer" {
+  project = var.project_id
+  role   = "roles/monitoring.viewer"
+  member = "serviceAccount:${google_service_account.node_service_account.email}"
+}
+
+resource "google_project_iam_member" "cluster_service_account-resourceMetadata-writer" {
+  project = var.project_id
+  role   = "roles/stackdriver.resourceMetadata.writer"
+  member = "serviceAccount:${google_service_account.node_service_account.email}"
+}
+
+resource "google_project_iam_member" "cluster_service_account-gcr" {
+  project = var.project_id
+  role   = "roles/storage.objectViewer"
+  member = "serviceAccount:${google_service_account.node_service_account.email}"
+}
+
+resource "google_project_iam_member" "cluster_service_account-artifact-registry" {
+  project = var.project_id
+  role   = "roles/artifactregistry.reader"
+  member = "serviceAccount:${google_service_account.node_service_account.email}"
+}
+
+resource "google_project_iam_member" "cluster_service_account-tpu" {
+  project = var.project_id
+  role   = "roles/tpu.admin"
+  member = "serviceAccount:${google_service_account.node_service_account.email}"
+}
+
+resource "google_compute_firewall" "webhook_admission_firewall" {
+  name    = "webhook-admission"
+  network = var.vpc_id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8443"]
+  }
+
+  source_ranges = ["10.0.1.0/28"]
+
+  target_service_accounts = [google_service_account.node_service_account.email]
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
-    name       = "${var.project}-node-pool"
-    cluster    = google_container_cluster.primary.name
-    node_count = 2
+  name    = "${var.name}-node-pool"
+  cluster = google_container_cluster.primary.name
 
-    location = var.zone
-    project = google_project.project.project_id
+  autoscaling {
+    max_node_count = var.max_nodes
+    min_node_count = var.min_nodes
+  }
 
-    node_config {
-        preemptible  = true
-        machine_type = "n1-standard-1"
-        metadata = {
-            disable-legacy-endpoints = "true"
-        }
+  location = var.zone
 
-        oauth_scopes = [
-            "https://www.googleapis.com/auth/logging.write",
-            "https://www.googleapis.com/auth/monitoring",
-        ]
-    }
+  node_config {
+    preemptible  = true
+    machine_type = var.machine_type
+
+    service_account = google_service_account.node_service_account.email
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
 }
+
+resource "google_container_node_pool" "gpu_preemptible_nodes" {
+  name    = "${var.name}-gpu-pool"
+  cluster = google_container_cluster.primary.name
+
+  autoscaling {
+    max_node_count = var.gpu_max_nodes
+    min_node_count = var.gpu_min_nodes
+  }
+
+  location = var.zone
+
+  node_config {
+    preemptible  = true
+    machine_type = var.gpu_machine_type
+
+    service_account = google_service_account.node_service_account.email
+
+    guest_accelerator {
+      count = 1
+      type  = var.accelerator_type
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
+
--- variables.tf
+++ variables.tf
@@ -1,15 +1,33 @@
-variable "project" {
-    default = "project"
+variable "project_id" {}
+
+variable "name" {}
+
+variable "region" {}
+
+variable "zone" {}
+
+variable "vpc_id" {}
+
+variable "subnet_id" {}
+
+variable "accelerator_type" {}
+
+variable "machine_type" {}
+
+variable "min_nodes" {
+  default = 1
 }
 
-variable "billing_acccount" {
-    default = ""
+variable "max_nodes" {
+  default = 5
 }
 
-variable "location" {
-    default = "us-east1"
+variable "gpu_machine_type" {}
+
+variable "gpu_min_nodes" {
+  default = 0
 }
 
-variable "zone" {
-    default = "us-east1-b"
+variable "gpu_max_nodes" {
+  default = 1
 }
